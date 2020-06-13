564ed486835f0e13201c78034b1c4d84b9c4d3f1
Mousa Zourob <mousazourob@gmail.com>
2020-06-13 13:16:25 -0400

    Added new text file for scores and made getwpm a function

--- GeneratePrompt.py
+++ GeneratePrompt.py
@@ -1,5 +1,4 @@
 import random
-import time
 import math
 
 
@@ -23,10 +22,48 @@ def get_prompt(words, length):  # takes in a list, 'dictionary', and returns a l
     return prompt
 
 
-file = "default.txt"  # Initializes file name to be used, better as variable so user can pick later
-dictionary = get_dictionary(file)  # Gets all words from text file
+def get_wpm():
+    import time
 
-prompt_length = 40 # Characters in the prompt
+    char_counter = 0  # counter to count how many correct characters
+    t0 = time.time()  # start time for user to type prompt
+    for index in range(0, len(prompt)):  # loops through every word in the prompt list
+        temp = input("Enter word: ")  # gets the current word
+
+        for i in range(0, len(temp)):  # loops through every letter in user input
+            if str(temp[i]).lower() == str((prompt[index])[i]):  # compares said letter to prompt letter
+                char_counter += 1  # adds 1 to correct letter counter
+            else:
+                # misspelt_letters((prompt[index])[i])
+                break  # if incorrect stop counting letters(add accuracy checker here)
+
+    t1 = time.time()  # gets time it took for them to finish prompt
+    time = (t1 - t0) / 60  # time is in seconds, divide by 60 to get mins
+    wpm = char_counter / 5 / time  # calculate wpm
+    wpm = math.ceil(wpm)  # round up wpm
+    return wpm
+
+'''
+def misspelt_letters(letter):
+    misspelt_file = "misspelt.txt"
+    readMis = open(misspelt_file, "r+")
+    misspeltChar = letter
+
+    with open(misspelt_file, 'r') as file:
+        lines = file.readlines()
+
+    for i in range(0, len(lines)-1):
+        if readMis.readlines()[i] == misspeltChar:
+            counterMis = int(readMis[i+1]) + 1
+            print(counterMis)
+
+    readMis.close()     
+'''
+
+words = "default.txt"  # Initializes file name to be used, better as variable so user can pick later
+dictionary = get_dictionary(words)  # Gets all words from text file
+
+prompt_length = 40  # Characters in the prompt
 prompt = get_prompt(dictionary, prompt_length)  # Generates a random prompt
 print(prompt)
 
@@ -34,19 +71,5 @@ print(prompt)
 # if keyboard.is_pressed(firstLetter):
 #     t0 = time.time()
 
-char_counter = 0    # counter to count how many correct characters
-t0 = time.time()    # start time for user to type prompt
-for index in range(0, len(prompt)):     # loops through every word in the prompt list
-    temp = input("Enter word: ")    # gets the current word
 
-    for i in range(0, len(temp)):   # loops through every letter in user input
-        if str(temp[i]).lower() == str((prompt[index])[i]):     # compares said letter to prompt letter
-            char_counter += 1   # adds 1 to correct letter counter
-        else:
-            break   # if incorrect stop counting letters(add accuracy checker here)
-
-t1 = time.time()    # gets time it took for them to finish prompt
-time = (t1 - t0)/60     # time is in seconds, divide by 60 to get mins
-wpm = char_counter/5/time   # calculate wpm
-print(math.ceil(wpm))   # print wpm
-
+print(get_wpm())  # print wpm
