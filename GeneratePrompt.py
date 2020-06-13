d7094f8abe2283ae6714a9eafc03147d074b9276
Mousa Zourob <mousazourob@gmail.com>
2020-06-13 10:12:40 -0400

    Fixed variable names

--- GeneratePrompt.py
+++ GeneratePrompt.py
@@ -11,12 +11,12 @@ def get_dictionary(file):  # takes in a file path path and returns a list, conte
 
 def get_prompt(words, length):  # takes in a list, 'dictionary', and returns a list, 'prompt'
     prompt = []
-    currentLength = 0
+    current_length = 0
 
-    while currentLength < length:
+    while current_length < length:
         word = words[random.randrange(0, len(words))]
         prompt.append(word[0:-1])
-        currentLength += len(word)
+        current_length += len(word)
 
     return prompt
 
@@ -24,6 +24,6 @@ def get_prompt(words, length):  # takes in a list, 'dictionary', and returns a l
 file = "default.txt"  # Initializes file name to be used, better as variable so user can pick later
 dictionary = get_dictionary(file)  # Gets all words from text file
 
-promptLength = 150  # Characters in the prompt
-prompt = get_prompt(dictionary, promptLength)  # Generates a random prompt
+prompt_length = 150  # Characters in the prompt
+prompt = get_prompt(dictionary, prompt_length)  # Generates a random prompt
 print(prompt)
