fc04a81dbe1fe4b874550cc5709ed14e63777407
Ellen Wang <ellenw2002@gmail.com>
2020-06-13 14:36:04 -0400

    Added two functions, one for calculating the % accuracy to 1 decimal place and another to find the actual amount of characters are in the prompt, also prints the accuracy

--- GeneratePrompt.py
+++ GeneratePrompt.py
@@ -22,6 +22,20 @@ def get_prompt(words, length):  # takes in a list, 'dictionary', and returns a l
 
     return prompt
 
+def get_accuracy(characters, length): # Takes in the number of accurate keypresses and the total number of characters, returns the % accuracy
+    accuracy = (characters/length)*100
+    round(accuracy, 1)
+
+    return accuracy
+
+def total_characters(array):
+    count_prompt = 0 # defines the variable and equates it to 0
+
+    for word in range(0, len(array)): # loops through every word in the prompt array
+        for x in range(0, len(array[word])): #loops through every character in each word of the array
+            count_prompt = count_prompt + 1 # every character adds 1 to the counter
+
+    return count_prompt
 
 file = "default.txt"  # Initializes file name to be used, better as variable so user can pick later
 dictionary = get_dictionary(file)  # Gets all words from text file
@@ -45,8 +59,13 @@ for index in range(0, len(prompt)):     # loops through every word in the prompt
         else:
             break   # if incorrect stop counting letters(add accuracy checker here)
 
+total_char_count = total_characters(prompt) # calls the method and returns the total characters count and stores it in total_char_count variable
+
+accuracy = get_accuracy(char_counter, total_char_count) #gets the % accuracy from the method
+
 t1 = time.time()    # gets time it took for them to finish prompt
 time = (t1 - t0)/60     # time is in seconds, divide by 60 to get mins
 wpm = char_counter/5/time   # calculate wpm
 print(math.ceil(wpm))   # print wpm
 
+print(accuracy) # prints the accuracy of the user
